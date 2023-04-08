74357dd68cb1f1b1229ec592e5cc0a182bdc1cde
procoderjake <130212848+procoderjake@users.noreply.github.com>
2023-04-08 15:06:09 +0530

    Updated files for version 1.0

--- ImageMerge.BuildV1.0/src/Main.java
+++ ImageMerge.BuildV1.0/src/Main.java
@@ -1,9 +1,68 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+
 public class Main {
+
 	public static void main(String[] args) {
-		User user = new User();
-		user.name = "Jake";
-		user.pass = "1234";
+		Scanner sc = new Scanner(System.in);
+		//Image Merge By Orca Corp.
+		//Build Version 0.1
+		//Some Errors may occur as it is still in beta
+		System.out.println();
+		System.out.println("Enter path to Backgrounds folder : ");
+		String path_BG = sc.nextLine();
+		System.out.println("Enter path to foregrounds folder : ");
+		String path_FG = sc.nextLine();
+		System.out.println("Enter path to output folder : ");
+		String outPath = sc.nextLine();
 		
+		File folder_BG = new File(path_BG);
+		File folder_FG = new File(path_FG);
+		
+		File[] array_BG = folder_BG.listFiles();
+		File[] array_FG = folder_FG.listFiles();
+		
+		for(int i = 0; i < array_BG.length; i++) {
+			for(int j = 0; j < array_FG.length; j++) {
+				int folder = i+1;
+				String fileName = outPath + "\\" + folder + "\\" + array_BG[i].getName() + " _ " + array_FG[j].getName();
+				try {
+					mergeImage(array_BG[i].getAbsolutePath(), array_FG[j].getAbsolutePath(), fileName);
+					System.out.println("File Created : " + fileName);
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+				
+				
+			}
+		}
+		sc.close();
+		
+	}
+	
+	static void mergeImage(String img_path_bg, String img_path_fg, String name) {
+		try {
+			
+			BufferedImage image_BG = ImageIO.read(new File(img_path_bg));
+			BufferedImage image_FG = ImageIO.read(new File(img_path_fg));
+			
+			int width_BG = image_BG.getWidth();
+			int height_BG = image_BG.getHeight();
+			
+			BufferedImage outputIMG = new BufferedImage(width_BG, height_BG, BufferedImage.TYPE_INT_ARGB);
+			Graphics2D render = outputIMG.createGraphics();
+			render.drawImage(image_BG, 0, 0, null);
+			render.drawImage(image_FG, 0, 0, null);
+			
+			ImageIO.write(outputIMG, "PNG", new File(name + ".png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
